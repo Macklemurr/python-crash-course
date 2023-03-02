914e837f73bb9a48cc432257e79d9806700f8fa0
Macklemurr <makacervo@gmail.com>
2023-03-02 14:27:04 -0500

    Removed celsius to fahrenheit.py - not relevant

--- Celsius to Fahrenheit.py
+++
@@ -1,7 +0,0 @@
-classA = 15
-classB = 12
-classC = 9
-soldA = input("How many Class A tickets sold? ")
-soldB = input("How many Class B tickets sold? ")
-soldC = input("How many Class C tickets sold? ")
-
